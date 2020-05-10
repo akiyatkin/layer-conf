9b4821d244269942f3395c2f95798840578995a3
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-10 05:42:46 +0400

    fix

--- infra.js
+++ infra.js
@@ -1,3 +1,7 @@
+import { Event } from '/vendor/infrajs/event/Event.js'
+import { Template } from '/vendor/infrajs/template/Template.js'
+import { Config } from '/vendor/infrajs/config/Config.js'	
+
 Event.one('Controller.oninit', function () {
 	Template.scope['~conf'] = Config.get();
 });
